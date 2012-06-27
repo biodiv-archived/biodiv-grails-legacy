f705f129e98c2681bf963efe29289079206e308a
sandeeptandekar <sandeept@strandls.com>
2012-06-27 15:28:22 +0530

    code changes to import remaining taxon_concept, synonyms and common names

--- grails-app/services/speciespage/NamesLoaderService.groovy
+++ grails-app/services/speciespage/NamesLoaderService.groovy
@@ -48,19 +48,19 @@ class NamesLoaderService {
 			//TODO:Handle cascading delete recommendations
 			recommendationService.deleteAll();
 		}
-
+		
 		int limit = BATCH_SIZE, offset = 0, noOfNames = 0;
 		def recos = new ArrayList<Recommendation>();
 		def conn = new Sql(sessionFactory.currentSession.connection())
+		def tmpTableName = "tmp_taxon_concept"
+		conn.executeUpdate("CREATE TABLE " + tmpTableName +  " as select t.name as name, t.canonical_Form as canonicalForm, t.normalized_Form as normalizedForm, t.binomial_Form as binomialForm, t.id as id from Taxonomy_Definition as t left outer join recommendation r on t.id = r.taxon_concept_id where r.name is null order by t.id ");
 		while(true) {
+			def taxonConcepts = conn.rows("select name, canonicalForm, normalizedForm, binomialForm, id from " + tmpTableName + " order by id limit " + limit + " offset " + offset);
 			//def taxonConcepts = TaxonomyDefinition.findAll("from TaxonomyDefinition as taxonConcept where taxonConcept not in (select reco.taxonConcept from Recommendation as reco) and taxonConcept.rank >= :minRank", [minRank:minRankToImport])
 			//def taxonConcepts = TaxonomyDefinition.findAllByRankGreaterThanEquals(minRankToImport);
 			//TODO: select returning different loop in every iteration so using order by. find the reaosn and remove this
-			def taxonConcepts = conn.rows("select t.name as name, t.canonical_Form as canonicalForm, t.normalized_Form as normalizedForm, t.binomial_Form as binomialForm, t.id as id from Taxonomy_Definition as t left outer join recommendation r on t.id = r.taxon_concept_id where r.name is null order by t.id limit "+limit+" offset "+offset);
-			
-
+		
 			taxonConcepts.each { taxonConcept ->
-
 				if(taxonConcept.name) {
 					def taxonObj = TaxonomyDefinition.get(taxonConcept.id)
 					recos.add(new Recommendation(name:taxonConcept.canonicalform, taxonConcept:taxonObj));
@@ -77,10 +77,10 @@ class NamesLoaderService {
 			recommendationService.save(recos);
 			recos.clear();
 			offset = offset + limit;
+			
 			if(!taxonConcepts) break; //no more results;
 		}
-		
-
+		conn.executeUpdate("DROP TABLE IF EXISTS " + tmpTableName);	
 		return noOfNames;
 	}
 
@@ -93,8 +93,12 @@ class NamesLoaderService {
 		def recos = new ArrayList<Recommendation>();
 		int offset = 0, noOfNames = 0, limit = BATCH_SIZE;
 		def conn = new Sql(sessionFactory.currentSession.connection())
+		
+		def tmpTableName = "tmp_synonyms"
+		conn.executeUpdate("CREATE TABLE " + tmpTableName +  " as select n.canonical_form as canonical_form, n.taxon_concept_id as taxonConcept from synonyms n left outer join recommendation r on n.canonical_form = r.name and n.taxon_concept_id = r.taxon_concept_id where r.name is null and n.canonical_form is not null group by n.canonical_form, n.taxon_concept_id order by n.taxon_concept_id");
+		
 		while(true) {
-			def synonyms = conn.rows("select n.canonical_form as canonical_form, n.taxon_concept_id as taxonConcept from synonyms n left outer join recommendation r on n.canonical_form = r.name and n.taxon_concept_id = r.taxon_concept_id where r.name is null and n.canonical_form is not null order by n.id limit "+limit+" offset "+offset)
+			def synonyms = conn.rows("select canonical_form, taxonConcept from " + tmpTableName + " order by taxonConcept limit " + limit + " offset " + offset);
 			//def synonyms = Synonyms.findAll("from Synonyms as synonym left join Recommendation as recommendation with synonym.name = recommendation.name and synonym.taxonConcept = recommendation.taxonConcept where r.name is null)", [max:NAME_BATCH_TO_LOAD, offset:offset]);
 			
 			synonyms.each { synonym ->
@@ -107,6 +111,8 @@ class NamesLoaderService {
 			recos.clear();
 			if(!synonyms) break;
 		}
+		
+		conn.executeUpdate("DROP TABLE IF EXISTS " + tmpTableName);
 		log.info "Imported synonyms into recommendations : "+noOfNames
 		return noOfNames;
 	}
@@ -120,8 +126,11 @@ class NamesLoaderService {
 		def recos = new ArrayList<Recommendation>();
 		int offset = 0, noOfNames = 0, limit=BATCH_SIZE;
 		def conn = new Sql(sessionFactory.currentSession.connection())
+		def tmpTableName = "tmp_common_names"
+		conn.executeUpdate("CREATE TABLE " + tmpTableName +  " as select n.name as name, n.taxon_concept_id as taxonConcept, n.language_id as language from common_names n left outer join recommendation r on n.name = r.name and n.taxon_concept_id = r.taxon_concept_id and n.language_id = r.language_id where r.name is null group by n.name, n.taxon_concept_id, n.language_id, n.id order by n.taxon_concept_id");
+		
 		while(true) {
-			def commonNames = conn.rows("select n.name as name, n.taxon_concept_id as taxonConcept, n.language_id as language from common_names n left outer join recommendation r on n.name = r.name and n.taxon_concept_id = r.taxon_concept_id where r.name is null group by n.name, n.taxon_concept_id, n.language_id, n.id order by n.id limit "+limit+" offset "+offset)
+			def commonNames = conn.rows("select name, taxonConcept, language from " + tmpTableName + " order by taxonConcept limit "+limit+" offset "+offset)
 			commonNames.each { cName ->
 				recos.add(new Recommendation(name:cName.name, isScientificName:false, languageId:cName.language, taxonConcept:TaxonomyDefinition.get(cName.taxonconcept)));
 				noOfNames++
@@ -132,6 +141,7 @@ class NamesLoaderService {
 			if(!commonNames) break;
 		}
 		recommendationService.save(recos);
+		conn.executeUpdate("DROP TABLE IF EXISTS " + tmpTableName);
 		log.info "Imported common names into recommendations : "+noOfNames
 		return noOfNames
 	}
