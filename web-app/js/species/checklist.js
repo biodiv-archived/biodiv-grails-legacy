0d82a0fed73fb4e3e8380b5bb272c6ec670201de
sandeeptandekar <sandeept@strandls.com>
2012-12-22 16:35:53 +0530

    some code changes

--- web-app/js/species/checklist.js
+++ web-app/js/species/checklist.js
@@ -2,16 +2,19 @@ function showChecklistMapView() {
 	$('#checklist_list_map').slideToggle(function() {
 	
 		if ($(this).is(':hidden')) {
-			$('div.checklist_list_main > div.checklist_list').show();
+			//$('div.checklist_list_main > div.checklist_list').show();
 			$('#map_view_bttn').css('background-color', 'transparent');
 			$('#map_results_list > div.checklist_list').remove();
-			updateGallery(undefined, undefined, 0, undefined, window.params.isGalleryUpdate);
+			//updateGallery(undefined, undefined, 0, undefined, window.params.isGalleryUpdate);
 			
-		} else {
-			$('div.checklist_list_main > div.checklist_list').hide();
-			$('div.checklist_list_main > div.checklist_list').html('');
-			$('div.paginateButtons').hide();
 		}
+		/*
+		else {
+			//$('div.checklist_list_main > div.checklist_list').hide();
+			//$('div.checklist_list_main > div.checklist_list').html('');
+			//$('div.paginateButtons').hide();
+		}
+		*/
 		google.maps.event.trigger(big_map, 'resize');
 		big_map.setCenter(nagpur_latlng);
 	});
@@ -68,8 +71,8 @@ function updateListPage(activeTag) {
 	return function (data) {
 		$('.checklist_list_main').replaceWith(data.checklistListHtml);
 		$('.info-message').replaceWith(data.checklistMsgtHtml);
+		$('#map_results_list').replaceWith(data.checklistMapHtml);
 		//$('#tags_section').replaceWith(data.tagsHtml);
-		//$('.observation_location_wrapper').replaceWith(data.mapViewHtml);
 		//setActiveTag(activeTag);
 		eatCookies();
 	}
