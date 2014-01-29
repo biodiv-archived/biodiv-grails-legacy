e54a8a71cd0607960af4b93d00e44c672ded52f5
pearlsravanthi <sravanthi@strandls.com>
2014-01-29 17:21:24 +0530

    removed ibp domain condition fr fb auth

--- src/groovy/species/auth/FacebookAuthUtils.groovy
+++ src/groovy/species/auth/FacebookAuthUtils.groovy
@@ -134,7 +134,7 @@ class FacebookAuthUtils {
 
 		if(domain.equals(grailsApplication.config.wgp.domain)) {
 			return grailsApplication.config.speciesPortal.wgp.facebook.appId;
-		} else if(domain.equals(grailsApplication.config.ibp.domain)) {
+		} else {//if(domain.equals(grailsApplication.config.ibp.domain)) {
 			return grailsApplication.config.speciesPortal.ibp.facebook.appId;
 		}
 		return null;
@@ -147,7 +147,7 @@ class FacebookAuthUtils {
 
 		if(domain.equals(grailsApplication.config.wgp.domain)) {
 			return grailsApplication.config.speciesPortal.wgp.facebook.secret
-		} else if(domain.equals(grailsApplication.config.ibp.domain)) {
+		} else { //if(domain.equals(grailsApplication.config.ibp.domain)) {
 			return grailsApplication.config.speciesPortal.ibp.facebook.secret
 		}
 		return null;
