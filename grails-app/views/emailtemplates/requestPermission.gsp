4a479973b195acaf0d22df6b628ea7a36b5e74f4
rksinha <rahul.iithyd@gmail.com>
2014-02-28 17:26:20 +0530

    Added missing files

---
+++ grails-app/views/emailtemplates/requestPermission.gsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html"%>
+
+Hi ${curator.name.capitalize()},
+<br/><br/>
+<br/> 
+
+Please <a href="${uri}" title="Confirmation code">click here</a> to confirm the curatorship.<br/>
+<br/><br/>
+Thank you,<br/>
+The Portal Team
---
+++ web-app/js/species/speciesPermission.js
@@ -0,0 +1,61 @@
+var curators_autofillUsersComp;
+
+$('#inviteCuratorsDialog').modal({
+    "show" : false,
+    "backdrop" : "static"
+});
+
+$('#inviteCurators').click(function(){
+    //console.log("checking logged in");
+    $.ajax({ 
+        url:window.params.isLoggedInUrl,
+        success: function(data, statusText, xhr, form) {
+        if(data === "true"){
+            $('#curatorUserIds').val('');
+            $('#userAndEmailList_3').val('');
+            $(curators_autofillUsersComp[0]).parent().children('li').each(function(){
+                $(curators_autofillUsersComp[0]).removeChoice($(this).find('span')[0]);
+            });
+            $('#inviteCuratorsForm')[0].reset()
+            $('#inviteCuratorsDialog').modal('show');
+            return false;
+        }else{
+            window.location.href = window.params.loginUrl+"?spring-security-redirect="+window.location.href;
+        }
+    },
+    error:function (xhr, ajaxOptions, thrownError){
+        return false;
+    } 
+    });
+});
+
+$("#inviteCuratorButton").click(function(){
+    $('#curatorUserIds').val(curators_autofillUsersComp[0].getEmailAndIdsList().join(","));
+    var selectedNodes = $(".taxDefIdCheck:checked").map(function() {return $(this).parent("span").find(".taxDefIdVal").val();}).get().join();
+    $('#inviteCuratorsForm').ajaxSubmit({ 
+        url:window.params.inviteCuratorsFormUrl,
+        dataType: 'json', 
+        clearForm: true,
+        resetForm: true,
+        type: 'POST',
+        data:{message:$('#inviteCuratorMsg').val(), selectedNodes : selectedNodes},
+        success: function(data, statusText, xhr, form) {
+            if(data.statusComplete) {
+                $('#inviteCuratorsDialog').modal('hide');
+                console.log("====RETURNED MSG ======" + data.msg);
+                $(".alertMsg").removeClass('alert alert-error').addClass('alert alert-success').html(data.msg);
+            } else {
+                $("#invite_CuratorMsg").removeClass('alert alert-error').addClass('alert alert-success').html(data.msg);
+            }
+            $('#inviteCuratorsForm')[0].reset()
+        }, error:function (xhr, ajaxOptions, thrownError){
+            //successHandler is used when ajax login succedes
+            var successHandler = this.success;
+            handleError(xhr, ajaxOptions, thrownError, successHandler, function() {
+                var response = $.parseJSON(xhr.responseText);
+
+            });
+            $('#inviteCuratorsForm')[0].reset()
+        } 
+    });	
+});
