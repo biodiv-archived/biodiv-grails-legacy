9e612b1934f7be1ea526fae34a68e6137200ee17
rksinha <rahul.iithyd@gmail.com>
2014-04-10 12:36:50 +0530

    Minor

--- userscripts/speciesImageDbMigration.groovy
+++ userscripts/speciesImageDbMigration.groovy
@@ -11,7 +11,8 @@ UPDATE resource SET context ='OBSERVATION' where id in (select resource_id from 
 UPDATE observation SET is_locked = false;
 
 
-
+//initialize all display order in newsletter to zero
+UPDATE newsletter SET display_order = 0;
 
 //set displayOrder for all news letter user group specific 
 // it should start from 0 and increase with date ascending
@@ -19,14 +20,14 @@ UPDATE observation SET is_locked = false;
 
 //this is for parent portal
 
-def res = Newsletter.executeQuery ('''
+def res1 = Newsletter.executeQuery ('''
     from Newsletter nl where nl.userGroup is null order by nl.date asc''', []);
 
     println "====================res ============== " + res
-    def counter = 0
-    res.each{
+    def counter1 = 0
+    res1.each{
         it.displayOrder = counter
-        counter++
+        counter1++
         if(!it.save(flush:true)){
             it.errors.allErrors.each { log.error it }        
         }
