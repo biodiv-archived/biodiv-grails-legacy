cfd24477a0061ad11b3ac7e21827a66214e5ccde
rksinha <rahul.iithyd@gmail.com>
2014-04-16 18:19:21 +0530

    Prints removed

--- grails-app/taglib/species/ObservationTagLib.groovy
+++ grails-app/taglib/species/ObservationTagLib.groovy
@@ -368,7 +368,6 @@ class ObservationTagLib {
                     }
                 }
                 resCount = resList.size()
-                println "=======RES LIST ====== " + resList
             break
     
         }
