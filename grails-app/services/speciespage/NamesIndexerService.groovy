65f1ec9f35ddb7a35a8d7f2ac4cd07330110a3dd
sathishinlinux <sathishinlinux@gmail.com>
2014-06-23 16:02:44 +0530

    Rebuild Action Comment released

--- grails-app/services/speciespage/NamesIndexerService.groovy
+++ grails-app/services/speciespage/NamesIndexerService.groovy
@@ -39,7 +39,7 @@ class NamesIndexerService {
 	 */
 	void rebuild() {
 
-		/*
+		
 		log.info "Publishing names to autocomplete index";
 		Lookup lookup1 = new TSTLookup();
 
@@ -76,7 +76,7 @@ class NamesIndexerService {
 		def indexStoreDir = grailsApplication.config.speciesPortal.nameSearch.indexStore;
 		store(indexStoreDir);
 
-		*/
+		
 	}
 
 	/**
