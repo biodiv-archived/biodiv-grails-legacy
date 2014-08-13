b820717a80fa61ad1518cca22dbe01a6c19f9510
sandeeptandekar <sandeept@strandls.com>
2014-08-13 17:53:40 +0530

    reorganization of accepted name, synonym and common name according to new data sturcture holding important col and other info.

---
+++ src/groovy/species/NameMetadata.groovy
@@ -0,0 +1,132 @@
+package species
+
+import species.auth.SUser
+
+abstract class NameMetadata extends NamesSorucedata {
+	
+	public enum NamePosition {
+		CLEAN("Clean"),
+		WORKING("Working"),
+		DIRTY("Dirty");
+
+		private String value;
+
+		NamePosition(String value) {
+			this.value = value;
+		}
+
+		static list() {
+			[
+				CLEAN,
+				WORKING,
+				DIRTY
+			]
+		}
+
+		String value() {
+			return this.value;
+		}
+
+		String toString() {
+			return this.value();
+		}
+	}
+	
+	public enum NameStatus {
+		ACCEPTED("Accepted"),
+		SYNONYM("Synonym"),
+		PROV_ACCEPTED("ProvAccepted"),
+		COMMON("Common");
+		private String value;
+
+		NameStatus(String value) {
+			this.value = value;
+		}
+
+		static list() {
+			[
+				ACCEPTED,
+				SYNONYM,
+				PROV_ACCEPTED,
+				COMMON
+			]
+		}
+
+		String value() {
+			return this.value;
+		}
+
+		String toString() {
+			return this.value();
+		}
+	}
+
+	public enum IbpSource {
+		SPECIES("Species"),
+		OBSERVATION("Observation"),
+		CURATOR("Curator")
+		
+		private String value;
+
+		IbpSource(String value) {
+			this.value = value;
+		}
+
+		static list() {
+			[
+				SPECIES,
+				OBSERVATION,
+				CURATOR
+			]
+		}
+
+		String value() {
+			return this.value;
+		}
+
+		String toString() {
+			return this.value();
+		}
+	}
+
+	
+	//String name
+	NameStatus status
+	NamePosition position = NamePosition.DIRTY
+	String authorYear
+	//Classification will be calculated from path
+	
+
+	String matchDatabaseName
+	String matchId
+
+	IbpSource ibpSource
+	String viaDatasource
+	
+	//to store reference backbone i.e col, butterfly by kunte default is col, ideally it should be enum
+	//String referenceTaxonomyBackbone = "COL"
+	
+	List<SUser> curators;
+	static hasMany = [curators: SUser]
+	
+	
+    static constraints = {
+		//name blank:false
+		authorYear nullable:true;
+		viaDatasource nullable:true;
+		ibpSource nullable:true;
+		matchDatabaseName nullable:true;
+		matchId nullable:true;
+	}
+
+    static mapping = {
+        tablePerHierarchy false
+    }
+
+	def beforeInsert(){
+	}
+	
+	def beforeUpdate(){
+	}
+
+}
---
+++ src/groovy/species/ScientificName.groovy
@@ -0,0 +1,115 @@
+package species
+
+abstract class ScientificName extends NameMetadata {
+
+	public enum RelationShip {
+		
+		AMBIGUOUS_SYNONYM("Ambiguous Synonym"),
+		ANAMORPH("Anamorph"),
+		BASIONYM("Basionym"),
+		HETEROTYPIC_SYNONYM("Heterotypic Synonym"),
+		HOMOTYPIC_SYNONYM("Homotypic Synonym"),
+		JUNIOR_SYNONYM("Junior Synonym"),
+		MISAPPLIED_NAME("Misapplied Name"),
+		NOMENCLATURAL_SYNONYM("Nomenclatural Synonym"),
+		OBJECTIVE_SYNONYM("Objective Synonym"),
+		SENIOR_SYNONYM("Senior Synonym"),
+		SUBJECTIVE_SYNONYM("Subjective Synonym"),
+		SYNONYM("Synonym"),
+		TELEOMORPH("Teleomorph"),
+		UNAVAILABLE_NAME("Unavailable Name"),
+		VALID_NAME("Valid Name"),
+		ACCEPTED_NAME("Accepted Name"),
+		PROTONYM("Protonym"),
+		OTHERS("Other");
+		
+		private String value;
+		
+		RelationShip(String value) {
+			this.value = value;
+		}
+		
+		String value() {
+			return this.value;
+		}
+	
+		static def toList() {
+	        return [
+	            AMBIGUOUS_SYNONYM,
+	            ANAMORPH,
+	            BASIONYM,
+	            HETEROTYPIC_SYNONYM,
+	            JUNIOR_SYNONYM,
+	            MISAPPLIED_NAME,
+	            NOMENCLATURAL_SYNONYM,
+	            OBJECTIVE_SYNONYM,
+	            SENIOR_SYNONYM,
+	            SUBJECTIVE_SYNONYM,
+	            SYNONYM,
+	            TELEOMORPH,
+	            UNAVAILABLE_NAME,
+	            VALID_NAME,
+	            ACCEPTED_NAME,
+	            PROTONYM,
+	            OTHERS
+	
+	        ]
+	    }
+	}
+
+	public enum TaxonomyRank {
+		KINGDOM("Kingdom"),
+		PHYLUM("Phylum"),
+		CLASS("Class"),
+		ORDER("Order"),
+		SUPER_FAMILY("Super-Family"),
+		FAMILY("Family"),
+		SUB_FAMILY("Sub-Family"),
+		GENUS("Genus"),
+		SUB_GENUS("Sub-Genus"),
+		SPECIES("Species"),
+		INFRA_SPECIFIC_TAXA("infra-specific-taxa");
+	
+		private String value;
+	
+		TaxonomyRank(String value) {
+			this.value = value;
+		}
+	
+		static list() {
+			[
+				KINGDOM,
+				PHYLUM,
+				CLASS,
+				ORDER,
+				SUPER_FAMILY,
+				FAMILY,
+				SUB_FAMILY,
+				GENUS,
+				SUB_GENUS,
+				SPECIES,
+				INFRA_SPECIFIC_TAXA
+			]
+		}
+	
+		String value() {
+			return this.value;
+		}
+	
+		String toString() {
+			return this.value();
+		}
+	}
+
+	String canonicalForm;
+	String normalizedForm;
+	String italicisedForm;
+	String binomialForm;
+	
+    static constraints = {
+		normalizedForm nullable:true;
+		italicisedForm nullable:true;
+		binomialForm nullable:true;
+    }
+	
+}
