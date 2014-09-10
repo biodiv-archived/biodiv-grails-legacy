abab90518be90bbbf4c4792237608ddb601fbba6
rksinha <rahul.iithyd@gmail.com>
2014-09-10 18:40:59 +0530

    Bug fix for digest mails

--- grails-app/services/speciespage/ObservationService.groovy
+++ grails-app/services/speciespage/ObservationService.groovy
@@ -2013,6 +2013,7 @@ class ObservationService extends AbstractObjectService {
                 templateMap["serverURL"] =  grailsApplication.config.grails.serverURL
                 templateMap["siteName"] = grailsApplication.config.speciesPortal.app.siteName
                 templateMap["resourcesServerURL"] = grailsApplication.config.speciesPortal.resources.serverURL
+                templateMap["grailsApplication"] = grailsApplication
                 mailSubject = "Activity digest on " + otherParams["userGroup"].name
                 bodyView = "/emailtemplates/digest"
                 templateMap["digestContent"] = otherParams["digestContent"]
