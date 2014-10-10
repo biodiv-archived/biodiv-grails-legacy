92fedd571e3ea45ffb0357c3ffe79c23399d4056
sandeeptandekar <sandeept@strandls.com>
2014-10-10 19:05:39 +0530

    added dummy api for namelist curator interface

--- grails-app/controllers/species/NamelistController.groovy
+++ grails-app/controllers/species/NamelistController.groovy
@@ -3,4 +3,38 @@ package species
 class NamelistController {
 
     def index() { }
+	
+	
+	/**
+	 * input : taxon id of ibp 
+	 * @return A map which contain keys as dirty, clean and working list. Values of this key is again a map with key as name and id
+	 * 
+	 */
+	def getNamesFromTaxon(){
+		//[dirtyList:[name:'aa', id:11], workingList:[name:'aa', id:11]]	
+	}
+	
+	/**
+	 * input : taxon id of ibp
+	 * @return All detail like kingdom, order etc
+	 */
+	def getNameDetails(){
+		//[name:'aa', kingdom:'kk', .....]
+	
+	}
+	
+	/**
+	 * input : string name
+	 * @return list of map where each map represent one result
+	 */
+	def searchCol(){
+		//[[name:'aa', namestatus:'st', colId:34, rank:4, group:'plant', sourceDatabase:'sb'], [name:'bb', namestatus:'st', colId:34, rank:4, group:'plant', sourceDatabase:'sb']]
+	}
+	/**
+	 * input : col id
+	 * @return same as api getNameDetails
+	 */
+	def getColDetails(){
+		//same getNameDetails
+	}
 }
