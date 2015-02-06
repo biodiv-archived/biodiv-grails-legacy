ccb4a76322c8e420a98309c177cbd2eeda344fb4
sandeeptandekar <sandeept@strandls.com>
2015-02-06 18:30:23 +0530

    REFS :#546 minor

--- grails-app/domain/species/participation/ActivityFeed.groovy
+++ grails-app/domain/species/participation/ActivityFeed.groovy
@@ -501,7 +501,7 @@ class ActivityFeed {
 	}
 	
 	public static dailyStats(UserGroup ug=null){
-		def startDate = new Date().minus(50)
+		def startDate = new Date().minus(1)
 		DateGroovyMethods.clearTime(startDate)
 		startDate = new java.sql.Date(startDate.getTime())
 		
