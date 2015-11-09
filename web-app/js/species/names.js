e8bc6822b5a7a83d17bb0c370687053337906ef6
pearlsravanthi <sravanthi@strandls.com>
2015-11-09 13:11:00 +0530

    FIXES : #673 Selecting autocomplete name puts common name in sc name field.

--- web-app/js/species/names.js
+++ web-app/js/species/names.js
@@ -111,8 +111,8 @@ function initializeNameSuggestion() {
             return false;
         }, select: function( event, ui ) {
             $(this).val( ui.item.label.replace(/<.*?>/g,"") );
-            $(this).closest(".commonNameDiv").next().find(".canName").val( ui.item.value );
-            $(this).closest(".commonNameDiv").next().find(".recoName").val( ui.item.value );
+            $(this).closest(".commonNameDiv").next().find(".canName").val( ui.item.desc );
+            $(this).closest(".commonNameDiv").next().find(".recoName").val( ui.item.desc );
             if(ui.item.languageName !== null){
                 $(this).closest(".commonNameDiv").find(".languageComboBox").val(ui.item.languageName).attr("selected",true);
                 $(this).closest(".commonNameDiv").find(".languageComboBox").data('combobox').refresh();
