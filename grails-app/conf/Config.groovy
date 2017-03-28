f96b021c4e3bc1952aff9880063762a72ad0e112
pearlsravanthi <sravanthi@strandls.com>
2017-03-28 17:26:55 +0530

    REFS : #872 removed startStatDate for digest from usergroup create page .. which was not being used in digest code

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1739,4 +1739,4 @@ grails.assets.bundle=true
 grails.assets.minifyJs=false
 cble.logging = trueors.url.pattern = '/api/*'
 cors.headers = ['Access-Control-Allow-Origin': '*','Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, TRACE, OPTIONS']
-cors.enable.logging = true
+//cors.enable.logging = true
