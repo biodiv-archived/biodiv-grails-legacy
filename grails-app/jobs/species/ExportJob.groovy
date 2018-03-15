27f2794166b02b555666b492c8be58c9a70d1964
Sravanthi <pearlsravanthi@gmail.com>
2018-03-15 17:17:36 +0530

    Update ExportJob.groovy

--- grails-app/jobs/species/ExportJob.groovy
+++ grails-app/jobs/species/ExportJob.groovy
@@ -106,8 +106,14 @@ class ExportJob {
 			return null
 		}
 		scheduledTaskList.each { DownloadLog dl ->
-			setStatus(dl, ObvUtilService.EXECUTING)
+			switch (dl.sourceType) {
+				case [OBSERVATION_LIST, UNIQUE_SPECIES]: 
+					return;
+				default :
+					setStatus(dl, ObvUtilService.EXECUTING);
+			}
 		}
+		
 		
 		return scheduledTaskList
 	}
